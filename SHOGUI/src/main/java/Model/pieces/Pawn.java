950cc76c963f2f19bdd232f649a2e9fabf87f8b2
Joan Estop <joan.estop@autonoma.cat>
2024-11-30 18:02:19 +0100

    implementació primera part isvalidmovePawn

--- SHOGUI/src/main/java/Model/pieces/Pawn.java
+++ SHOGUI/src/main/java/Model/pieces/Pawn.java
@@ -9,6 +9,17 @@ public class Pawn extends Piece {
 
     @Override
     public boolean isValidMove(int newX, int newY, Piece[][] board) {
-        return false;
+        int direction = isWhite ? 1 : -1;
+
+        boolean valid_move = false;
+        if((newX >= 0 && newX < 9)&&(newY >= 0 && newY < 9))
+        {
+            Piece targetPiece = board[newX][newY];
+            if ((newX == x && newY == y + direction) && (targetPiece == null || targetPiece.isWhite() != this.isWhite))
+            {
+                valid_move = true;
+            }
+        }
+        return valid_move;
     }
 }

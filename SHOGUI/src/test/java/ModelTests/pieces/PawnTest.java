b8fb5901a991a7d2662012812d5416601764ad9e
Joan Estop <joan.estop@autonoma.cat>
2024-11-30 17:50:15 +0100

    implementació tests de isvalidmove pawn

--- SHOGUI/src/test/java/ModelTests/pieces/PawnTest.java
+++ SHOGUI/src/test/java/ModelTests/pieces/PawnTest.java
@@ -1,5 +1,6 @@
 package ModelTests.pieces;
 
+import Model.Board;
 import Model.Piece;
 import Model.pieces.Pawn;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PawnTest {
+
+    public class MockBoard extends Board
+    {
+        private Piece[][] MockBoard = new Piece[9][9];
+        public MockBoard(){
+            initializeBoard();
+        }
+        public void initializeBoard(){
+            MockBoard[0][0] = new Pawn(0, 0, true);
+            MockBoard[0][8] = new Pawn(0, 8, true);
+            MockBoard[2][1] = new Pawn(2, 1, true);
+            MockBoard[3][1] = new Pawn(3, 1, true);
+            MockBoard[2][2] = new Pawn(2, 2, true);
+            MockBoard[3][2] = new Pawn(3, 2, false);
+            MockBoard[5][4] = new Pawn(5, 4, true);
+        }
+        public Piece getPiece(int x, int y) {
+            return MockBoard[x][y];
+        }
+    }
+
+
     @Test
     void testConstructorPawn() {
         Pawn whitePawn = new Pawn(0, 0, true);
@@ -46,4 +69,37 @@ public class PawnTest {
         assertThrows(IllegalArgumentException.class, () -> new Pawn(10, 10, true),
                 "El constructor hauria de llemçar excepcio valors molt positius");
     }
+
+    @Test
+    void testIsValidMove()
+    {
+        MockBoard taulell_auxiliar = new MockBoard();
+
+        Piece peo_00 = taulell_auxiliar.getPiece(0,0);
+        assertTrue(peo_00.isValidMove(0, 1, taulell_auxiliar.MockBoard), "no es mou bé endavant");
+        assertFalse(peo_00.isValidMove(0, -1, taulell_auxiliar.MockBoard), "permet sortir del tauler");
+        assertFalse(peo_00.isValidMove(-1, 0, taulell_auxiliar.MockBoard), "permet sortir tauler i moures esquerre");
+
+        Piece peo_54 = taulell_auxiliar.getPiece(5,4);
+        assertFalse(peo_54.isValidMove(6, 4, taulell_auxiliar.MockBoard), "permet moviment horitzontal");
+        assertFalse(peo_54.isValidMove(4, 4, taulell_auxiliar.MockBoard), "permet moviment horitzontal");
+        assertFalse(peo_54.isValidMove(6, 5, taulell_auxiliar.MockBoard), "permet moviment diagonal");
+        assertFalse(peo_54.isValidMove(4, 5, taulell_auxiliar.MockBoard), "permet moviment diagonal");
+        assertFalse(peo_54.isValidMove(6, 3, taulell_auxiliar.MockBoard), "permet moviment diagonal");
+        assertFalse(peo_54.isValidMove(4, 3, taulell_auxiliar.MockBoard), "permet moviment diagonal");
+        assertFalse(peo_54.isValidMove(5, 6, taulell_auxiliar.MockBoard), "permet moviment doble");
+
+        Piece peo_08 = taulell_auxiliar.getPiece(0,8);
+        assertFalse(peo_08.isValidMove(0, 9, taulell_auxiliar.MockBoard), "permet sortir tauler");
+
+        Piece peo_21 = taulell_auxiliar.getPiece(2,1);
+        assertFalse(peo_21.isValidMove(2, 2, taulell_auxiliar.MockBoard), "permet menjar un altre peça blanca");
+
+        Piece peo_22 = taulell_auxiliar.getPiece(2,2);
+        assertTrue(peo_22.isValidMove(2, 3, taulell_auxiliar.MockBoard), "no permet menjar peça negra");
+
+    }
+
+
+
 }

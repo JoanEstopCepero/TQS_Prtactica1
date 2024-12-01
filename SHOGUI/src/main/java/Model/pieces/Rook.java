beb14f6d7c29f5f36e0b4674ee1c1d3cfee48540
Joan Estop <joan.estop@autonoma.cat>
2024-12-01 12:58:50 +0100

    implementem codi per no permetre sortir taulell

--- SHOGUI/src/main/java/Model/pieces/Rook.java
+++ SHOGUI/src/main/java/Model/pieces/Rook.java
@@ -6,6 +6,10 @@ public class Rook extends Piece{
     }
     @Override
     public boolean isValidMove(int newX, int newY, Piece[][] board){
+        if((newX >= 0 && newX < 9)&&(newY >= 0 && newY < 9))
+        {
+            return true;
+        }
         return false;
     }
 }

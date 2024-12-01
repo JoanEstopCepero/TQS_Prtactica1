44195813f128499240b0ba0d6fd2fbbb5275322c
Joan Estop <joan.estop@autonoma.cat>
2024-12-01 10:53:09 +0100

    implementem tests constructor knight

--- SHOGUI/src/test/java/ModelTests/pieces/KnightTest.java
+++ SHOGUI/src/test/java/ModelTests/pieces/KnightTest.java
@@ -1,4 +1,50 @@
 package ModelTests.pieces;
 
+import Model.Piece;
+import Model.pieces.Knight;
+import Model.pieces.Pawn;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class KnightTest {
+    @Test
+    void testConstructorKing() {
+        Knight whiteKnight = new Knight(0, 0, true);
+        assertEquals("N", whiteKnight.getName(), "fitxa incorrecte");
+        assertEquals(0, whiteKnight.getX(), "x incorrecte");
+        assertEquals(0, whiteKnight.getY(), "y incorrecte");
+        assertTrue(whiteKnight.isWhite(), "color incorrecte");
+
+        Knight blackKnight = new Knight(3, 6, false);
+        assertEquals("N", blackKnight.getName(), "fitxa incorrecte");
+        assertEquals(3, blackKnight.getX(), "x incorrecte");
+        assertEquals(6, blackKnight.getY(), "y incorrecte");
+        assertFalse(blackKnight.isWhite(), "color incorrecte");
+
+
+
+        //Com el tauler es de 9x9, les posicions no poden ser ni mes petites que 0
+        //ni mes grans que 8
+        assertThrows(IllegalArgumentException.class, () -> new Knight(-10, -10, true),
+                "El constructor hauria de llemçar excepcio valors molt negatius");
+
+        assertThrows(IllegalArgumentException.class, () -> new Knight(-1, -1, true),
+                "El constructor hauria de llemçar excepcio valors  negatius");
+
+        Piece piece5 = new Knight(5, 6, true);
+        assertEquals(5, piece5.getX(), "el constructor no permet valors de x del rang 0 - 8");
+        assertEquals(6, piece5.getY(), "el constructor no permet valors de y del rang 0 - 8");
+
+        Piece piece8 = new Knight(8, 8, true);
+        assertEquals(8, piece8.getX(), "el constructor no permet valors frontera de x");
+        assertEquals(8, piece8.getY(), "el constructor no permet valors frontera de y");
+
+        assertThrows(IllegalArgumentException.class, () -> new Knight(9, 9, true),
+                "El constructor hauria de llemçar excepcio valos positius");
+
+        assertThrows(IllegalArgumentException.class, () -> new Knight(10, 10, true),
+                "El constructor hauria de llemçar excepcio valors molt positius");
+    }
 }

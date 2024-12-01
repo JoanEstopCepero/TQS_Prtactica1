92b6229b535ed8283ecf87be38f33f0ed28ea856
Joan Estop <joan.estop@autonoma.cat>
2024-12-01 17:27:44 +0100

    creacio game controller i game controller tests

---
+++ SHOGUI/src/test/java/ControllerTests/GameControllerTests.java
@@ -0,0 +1,14 @@
+package ControllerTests;
+import Model.Board;
+
+public class GameControllerTests {
+    private final Board board;
+
+    public GameControllerTests(Board board) {
+        this.board = board;
+    }
+
+    public void startGame(){
+
+    }
+}

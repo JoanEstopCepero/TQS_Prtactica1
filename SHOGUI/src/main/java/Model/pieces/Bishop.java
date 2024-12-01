63f0c7d193dfd7691a50a07ccf7245fa97e5c406
Joan Estop <joan.estop@autonoma.cat>
2024-12-01 11:45:34 +0100

    implementacio codi per no permetre moviments fora tauler

--- SHOGUI/src/main/java/Model/pieces/Bishop.java
+++ SHOGUI/src/main/java/Model/pieces/Bishop.java
@@ -7,6 +7,10 @@ public class Bishop extends Piece {
     }
     @Override
     public boolean isValidMove(int newX, int newY, Piece[][] board){
-        return false;
+        if((newX >= 0 && newX < 9)&&(newY >= 0 && newY < 9))
+        {
+            return true;
+        }
+            return false;
     }
 }

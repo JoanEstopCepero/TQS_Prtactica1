b4b57a8529c7f5001fc60beaa6d05845a19b9f75
Joan Estop <joan.estop@autonoma.cat>
2024-12-01 11:23:43 +0100

    implementació test per negres i per menjar fixes

--- SHOGUI/src/test/java/ModelTests/pieces/KnightTest.java
+++ SHOGUI/src/test/java/ModelTests/pieces/KnightTest.java
@@ -24,11 +24,31 @@ public class KnightTest {
             MockBoardW[2][4] = new Knight(2, 4, true);
             MockBoardW[5][4] = new Knight(5, 4, true);
 
-            MockBoardW[6][2] = new Pawn(6, 2, false);
-            MockBoardW[4][2] = new Pawn(4, 2, true);
+            MockBoardW[6][6] = new Pawn(6, 6, false);
+            MockBoardW[4][6] = new Pawn(4, 6, true);
         }
         public Piece getPiece(int x, int y) {
             return MockBoardW[x][y];
+        }
+    }
+    public class MockBoardB extends Board
+    {
+        private Piece[][] MockBoardB = new Piece[9][9];
+        public MockBoardB(){
+            initializeBoard();
+        }
+        public void initializeBoard(){
+            MockBoardB[0][0] = new Knight(0, 0, false);
+            MockBoardB[8][8] = new Knight(8, 8, false);
+
+            MockBoardB[2][4] = new Knight(2, 4, false);
+            MockBoardB[5][4] = new Knight(5, 4, false);
+
+            MockBoardB[4][2] = new Pawn(4, 2, true);
+            MockBoardB[6][2] = new Pawn(6, 2, false);
+        }
+        public Piece getPiece(int x, int y) {
+            return MockBoardB[x][y];
         }
     }
     @Test
@@ -90,9 +110,37 @@ public class KnightTest {
 
         assertFalse(knight_24.isValidMove(1, 2, taulell_aux_w.MockBoardW), "permet moures enrere");
         assertFalse(knight_24.isValidMove(3, 2, taulell_aux_w.MockBoardW), "permet moures enrere");
-        assertFalse(knight_24.isValidMove(3, 4, taulell_aux_w.MockBoardW), "permet moviments incorrectes");
+        assertFalse(knight_24.isValidMove(4, 6, taulell_aux_w.MockBoardW), "permet moviments incorrectes");
         assertFalse(knight_24.isValidMove(2, 5, taulell_aux_w.MockBoardW), "permet moviments incorrectes");
 
+        Piece knight_54 = taulell_aux_w.getPiece(5,4);
+        assertTrue(knight_54.isValidMove(6, 6, taulell_aux_w.MockBoardW), "no permet menjar");
+        assertFalse(knight_54.isValidMove(4, 6, taulell_aux_w.MockBoardW), "permet menjar blanques");
 
+        MockBoardB taulell_aux_b = new MockBoardB();
+        Piece knight_00B = taulell_aux_b.getPiece(0,0);
+        assertFalse(knight_00B.isValidMove(-1, -1, taulell_aux_b.MockBoardB), "es mou fora taulell");
+        assertFalse(knight_00B.isValidMove(1, -1, taulell_aux_b.MockBoardB), "es mou fora taulell");
+        assertFalse(knight_00B.isValidMove(-1, 1, taulell_aux_b.MockBoardB), "es mou fora taulell");
+
+        Piece knight_88B = taulell_aux_b.getPiece(8,8);
+        assertFalse(knight_88B.isValidMove(8, 9, taulell_aux_b.MockBoardB), "es mou fora taulell");
+        assertFalse(knight_88B.isValidMove(9, 9, taulell_aux_b.MockBoardB), "es mou fora taulell");
+        assertFalse(knight_88B.isValidMove(7, 9, taulell_aux_b.MockBoardB), "es mou fora taulell");
+        assertTrue(knight_00B.isValidMove(7, 6, taulell_aux_b.MockBoardB), "no permet moviment correcte");
+
+
+        Piece knight_24B = taulell_aux_b.getPiece(2,4);
+        assertTrue(knight_24B.isValidMove(1, 2, taulell_aux_b.MockBoardB), "no permet moviment correcte");
+        assertTrue(knight_24B.isValidMove(3, 2, taulell_aux_b.MockBoardB), "no permet moviment correcte");
+
+        assertFalse(knight_24B.isValidMove(1, 6, taulell_aux_b.MockBoardB), "permet moures enrere");
+        assertFalse(knight_24B.isValidMove(3, 6, taulell_aux_b.MockBoardB), "permet moures enrere");
+        assertFalse(knight_24B.isValidMove(4, 6, taulell_aux_b.MockBoardB), "permet moviments incorrectes");
+        assertFalse(knight_24B.isValidMove(2, 5, taulell_aux_b.MockBoardB), "permet moviments incorrectes");
+
+        Piece knight_54B = taulell_aux_b.getPiece(5,4);
+        assertTrue(knight_54B.isValidMove(4, 2, taulell_aux_b.MockBoardB), "no permet menjar");
+        assertFalse(knight_54B.isValidMove(6, 2, taulell_aux_b.MockBoardB), "permet menjar negres");
     }
 }

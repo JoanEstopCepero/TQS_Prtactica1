1c16d8b29b84a29285f08b73e6f65f47da3fb0f6
Joan Estop <joan.estop@autonoma.cat>
2024-12-01 12:54:19 +0100

    implementacio test i codi constructor rook

--- SHOGUI/src/test/java/ModelTests/pieces/RookTests.java
+++ SHOGUI/src/test/java/ModelTests/pieces/RookTests.java
@@ -1,4 +1,50 @@
 package ModelTests.pieces;
 
+import Model.Piece;
+import Model.pieces.Rook;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class RookTests {
+    @Test
+    void testConstructorLance() {
+        Rook whiteRook = new Rook(0, 0, true);
+        assertEquals("R", whiteRook.getName(), "fitxa incorrecte");
+        assertEquals(0, whiteRook.getX(), "x incorrecte");
+        assertEquals(0, whiteRook.getY(), "y incorrecte");
+        assertTrue(whiteRook.isWhite(), "color incorrecte");
+
+        Rook blackRook = new Rook(3, 6, false);
+        assertEquals("R", blackRook.getName(), "fitxa incorrecte");
+        assertEquals(3, blackRook.getX(), "x incorrecte");
+        assertEquals(6, blackRook.getY(), "y incorrecte");
+        assertFalse(blackRook.isWhite(), "color incorrecte");
+
+
+
+        //Com el tauler es de 9x9, les posicions no poden ser ni mes petites que 0
+        //ni mes grans que 8
+        assertThrows(IllegalArgumentException.class, () -> new Rook(-10, -10, true),
+                "El constructor hauria de llemçar excepcio valors molt negatius");
+
+        assertThrows(IllegalArgumentException.class, () -> new Rook(-1, -1, true),
+                "El constructor hauria de llemçar excepcio valors  negatius");
+
+        Piece piece5 = new Rook(5, 6, true);
+        assertEquals(5, piece5.getX(), "el constructor no permet valors de x del rang 0 - 8");
+        assertEquals(6, piece5.getY(), "el constructor no permet valors de y del rang 0 - 8");
+
+        Piece piece8 = new Rook(8, 8, true);
+        assertEquals(8, piece8.getX(), "el constructor no permet valors frontera de x");
+        assertEquals(8, piece8.getY(), "el constructor no permet valors frontera de y");
+
+        assertThrows(IllegalArgumentException.class, () -> new Rook(9, 9, true),
+                "El constructor hauria de llemçar excepcio valos positius");
+
+        assertThrows(IllegalArgumentException.class, () -> new Rook(10, 10, true),
+                "El constructor hauria de llemçar excepcio valors molt positius");
+    }
+
 }

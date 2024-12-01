a9e997fee31a42613f55a0f9008129ff5bde0237
Joan Estop <joan.estop@autonoma.cat>
2024-12-01 11:34:37 +0100

    implementacio test i codi constructor

--- SHOGUI/src/test/java/ModelTests/pieces/BishopTest.java
+++ SHOGUI/src/test/java/ModelTests/pieces/BishopTest.java
@@ -1,4 +1,52 @@
 package ModelTests.pieces;
 
+import Model.Piece;
+import Model.pieces.Bishop;
+import Model.pieces.Pawn;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+
 public class BishopTest {
+    @Test
+    void testConstructorBishop() {
+        Bishop whiteBishop = new Bishop(0, 0, true);
+        assertEquals("B", whiteBishop.getName(), "fitxa incorrecte");
+        assertEquals(0, whiteBishop.getX(), "x incorrecte");
+        assertEquals(0, whiteBishop.getY(), "y incorrecte");
+        assertTrue(whiteBishop.isWhite(), "color incorrecte");
+
+        Bishop blackBishop = new Bishop(3, 6, false);
+        assertEquals("B", blackBishop.getName(), "fitxa incorrecte");
+        assertEquals(3, blackBishop.getX(), "x incorrecte");
+        assertEquals(6, blackBishop.getY(), "y incorrecte");
+        assertFalse(blackBishop.isWhite(), "color incorrecte");
+
+
+
+        //Com el tauler es de 9x9, les posicions no poden ser ni mes petites que 0
+        //ni mes grans que 8
+        assertThrows(IllegalArgumentException.class, () -> new Bishop(-10, -10, true),
+                "El constructor hauria de llemçar excepcio valors molt negatius");
+
+        assertThrows(IllegalArgumentException.class, () -> new Bishop(-1, -1, true),
+                "El constructor hauria de llemçar excepcio valors  negatius");
+
+        Piece piece5 = new Bishop(5, 6, true);
+        assertEquals(5, piece5.getX(), "el constructor no permet valors de x del rang 0 - 8");
+        assertEquals(6, piece5.getY(), "el constructor no permet valors de y del rang 0 - 8");
+
+        Piece piece8 = new Bishop(8, 8, true);
+        assertEquals(8, piece8.getX(), "el constructor no permet valors frontera de x");
+        assertEquals(8, piece8.getY(), "el constructor no permet valors frontera de y");
+
+        assertThrows(IllegalArgumentException.class, () -> new Bishop(9, 9, true),
+                "El constructor hauria de llemçar excepcio valos positius");
+
+        assertThrows(IllegalArgumentException.class, () -> new Bishop(10, 10, true),
+                "El constructor hauria de llemçar excepcio valors molt positius");
+    }
+
 }
